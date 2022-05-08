bbebf44c7411892c258a37c4e29508d1ccf14e24
Danisve <102831644+Danisve@users.noreply.github.com>
2022-05-07 19:17:17 -0600

    a

--- yaz.py
+++ yaz.py
@@ -10,10 +10,11 @@ st.markdown("![Alt Text](https://www.baumer.com/medias/sys_master/images-content
 st.subheader("Polarizador:")
 st.image("https://w7.pngwing.com/pngs/939/208/png-transparent-polarized-light-polarimeter-optical-rotation-density-matrix-bulbs-angle-experiment-light.png")
 st.write ("Los polarizdores son materiales que transmiten de forma selectiva las ondas electromagneticas de la luz, restringiendo las ondas a una sola direccion de propagacion bloqueando el resto de planos de polarizacion")
-genre = st.radio("Tipo de polarizacion", ('Lineal ', 'Circular', 'Elíptica'))
-if genre == 'Lineal':
+botones_vector = ['Lineal','Circular','Elíptica']
+vectores = st.sidebar.radio ('Tipo de polarizacion',botones_vector)
+if vectores == 'Lineal':
      st.write("El vector E traza sobre el plano perpendicular a la dirección de propagación una linea recta.")
-if genre == 'Circular':
-     st.write('Componentes de E misma magnitud, pero una diferencia de fase, puede verse girando a la izquierda o hacia la derecha.')
-if genre == 'Elíptica':
-     st.write('Componentes de E son diferentes, abarca cualquier diferencia de magnitud de fase de Ex y Ey, es el estado de polarización más general.')
+if vectores == 'Circular':
+    st.write('Componentes de E misma magnitud, pero una diferencia de fase, puede verse girando a la izquierda o hacia la derecha.')
+if vectores == 'Elíptica':
+    st.write('Componentes de E son diferentes, abarca cualquier diferencia de magnitud de fase de Ex y Ey, es el estado de polarización más general.') 
