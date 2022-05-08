0b5f1b0273e04e8467422b0f854e3955f6b7eed6
Danisve <102831644+Danisve@users.noreply.github.com>
2022-05-07 18:53:53 -0600

    gif

--- yaz.py
+++ yaz.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Polarización de la luz")
 st.write("La polarización es una propiedad de la luz, que describe la geometría interna de una fuente de radiación")
 st.subheader("La luz:")
-st.markdown("![Alt Text](http://rsefalicante.umh.es/TemasLuz/wave.gif)")
+st.markdown("![Alt Text](http://www.juliohernandezfotografia.cl/wp-content/uploads/2015/10/luz-polarizador.gif)")
 st.write("La luz son ondas electromagneticas, visible al ojo humano,estas ondas vibran en diferentes direcciones perpendiculares en forma de propagación hasta que estas son sometidas a la polarizacion")
 st.subheader("Luz polarizada:")
 st.write("las ondas de luz vibran en un solo plano, esto se produce gracias a la ayuda de un polarizador")
